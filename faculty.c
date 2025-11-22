11a866918f7519bbace7ee307ace14e2a43073a1
Durgaprasad Puvvala <prasad00015@gmail.com>
2025-11-22 16:05:27 +0530

    send

---
+++ faculty.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("Faculty Management System");
+        printf("\nManikant");
+        printf("\nBalu");
+        printf("\nchiru");
+        printf("\nPrasad");
+    return 0;
+}
